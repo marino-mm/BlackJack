6d9eaa29ae965469a2257bb0a2df26e72fa31280
Marino <marinovic.matej21@gmail.com>
2025-08-26 10:35:21 +0200

    Added new classes for easier use

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,7 @@
 import json
 from pathlib import Path
+from tkinter import N
+from typing import Dict, Set
 
 from fastapi import FastAPI, WebSocket, Response
 from fastapi.staticfiles import StaticFiles
@@ -16,50 +18,66 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class UserConnection:
+    def __init__(self, websocket: WebSocket):
+        self.connection = websocket
+        self.username = None
+        
+    async def create(self):
+        while not self.username:
+            message_dict = await self.connection.receive_json()
+            if message_dict.get('username'):
+                self.username = message_dict.get('username')
+    
+    def __eq__(self, value: object) -> bool:
+        return self.username == value.username
+    
+    def __hash__(self):
+        return hash(self.username)
+
+        
+
 class WebsocketManager:
-    def __init__(self):
-        self.active_connections = []
+    def __init__(self) -> None:
+        self.active_connections: Set[UserConnection] = set()
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        user_connection = UserConnection(websocket)
+        await user_connection.create()
+        self.active_connections.add(user_connection)
+        return user_connection
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, user_connection):
+        self.active_connections.discard(user_connection)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def send_personal_message(self, message: str, user_connection: UserConnection):
+        await user_connection.connection.send_text(message)
 
     async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+        for user_connection in self.active_connections:
+            await user_connection.connection.send_text(message)
 
-    async def broadcast_everyone_except_me(self, message: str, websocket: WebSocket):
-        for connection in self.active_connections:
-            if connection != websocket:
-                await connection.send_text(message)
+    async def broadcast_everyone_except_me(self, message: str, my_connection: UserConnection):
+        for user_connection in self.active_connections:
+            if user_connection != my_connection:
+                await user_connection.connection.send_text(message)
 
 websocket_manager = WebsocketManager()
 user_list = []
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket_manager.connect(websocket)
-    username = ''
+    user_connection = await websocket_manager.connect(websocket)
     try:
+        response_json = None
+        user_list.append(user_connection.username)
+        response_json = {"user_list" : user_list}
+        await websocket_manager.broadcast(json.dumps(response_json))
         while True:
-            json_massage = await websocket.receive_text()
-            json_data = json.loads(json_massage)
-            response_json = None
-            if json_data.get('username'):
-                username = json_data.get('username')
-                user_list.append(username)
-                response_json = {
-                    "user_list" : user_list,
-                }
-                await websocket_manager.broadcast(json.dumps(response_json))
+            json_data = await user_connection.connection.receive_json()
             if json_data.get('message'):
-                message = f"{username}: {json_data.get('message')}"
+                message = f"{user_connection.username}: {json_data.get('message')}"
                 response_json = {
                     "message": message,
                 }
@@ -67,25 +85,24 @@ async def websocket_endpoint(websocket: WebSocket):
             if json_data.get('cursor'):
                 data = str(json_data.get('cursor')).split(' ')
                 cursor_name = data[0]
-                x = int(data[1]) - 10
-                y = int(data[2]) - 10
-                html_div = f'<div style="width: 10px; height: 10px; background-color: black; position: absolute; top: {y}px; left: {x}px;"></div>'
+                x = int(data[1])
+                y = int(data[2])
+                html_div = f'<div id="{cursor_name}"style="width: 10px; height: 10px; background-color: blue; position: absolute; top: {y}px; left: {x}px;">cursor_name</div>'
                 response_json = {
-                        "cursor": html_div
+                        "cursor": html_div,
+                        "cursor_username": cursor_name
                     }
-                await websocket_manager.broadcast_everyone_except_me(json.dumps(response_json), websocket)
+                await websocket_manager.broadcast_everyone_except_me(json.dumps(response_json), user_connection)
     except WebSocketDisconnect:
         websocket_manager.disconnect(websocket)
-        user_list.remove(username)
-        response_json = {
-            "user_list": user_list,
-        }
+        user_list.remove(user_connection.username)
+        response_json = {"user_list": user_list}
         await websocket_manager.broadcast(json.dumps(response_json))
 
 @app.get("/heartbeat")
 def heart_bet():
     return Response()
 
-# static_dir = Path(__file__).resolve().parent.parent / "frontend_vanila" / "static" / "chat_room"
-static_dir = Path(__file__).resolve().parent.parent / "frontend"
+static_dir = Path(__file__).resolve().parent.parent / "frontend_vanila" / "static" / "chat_room"
+# static_dir = Path(__file__).resolve().parent.parent / "frontend"
 app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")
