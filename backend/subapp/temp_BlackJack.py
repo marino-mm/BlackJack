4cb61882c9487cea17e9fa4945eafd622a15b82e
Marino <marinovic.matej21@gmail.com>
2025-10-06 22:20:31 +0200

    Implelented first asyncio.Event

--- backend/subapp/temp_BlackJack.py
+++ backend/subapp/temp_BlackJack.py
@@ -1,5 +1,5 @@
 
-from asyncio import Queue, Task
+from asyncio import Event, Queue, Task
 from dataclasses import dataclass
 from typing import List, Optional, Set
 
@@ -13,6 +13,7 @@ class PlayerMessage():
     player: BlackJackPlayer
     game: BlackJackGame
     type: str
+    data: dict
 
 
 class BlackJackGame:
@@ -37,16 +38,27 @@ class BlackJackGame:
 
         self.running_tasks: Set[Task] = set()
 
+        self.continue_to_next_move = Event()
+
         self.game_status = "waiting"
 
     async def game_worker(self):
         while True:
-            message: PlayerMessage = self.game_queue.get()
-
+            message = await self.game_queue.get()
             if message.type == 'Join':
                 await self.add_player(message.player)
             elif message.type == 'Disconect':
                 await self.remove_player(message.player)
+            elif message.type == 'MoveSlots':
+                if message.data.get("new_slot_index") is not None:
+                    self.move_slot(message.data)
+                    # await self.send_slots()
+            elif message.type == 'PlayerAction':
+                if message.data.get("player", None) == self.active_player:
+                    valid_move = await self.poccess_players_move(message.data)
+                    if valid_move:
+                        # await self.send_slots()
+                        self.continue_to_next_move.set()
 
     async def add_player(self, player: BlackJackPlayer):
         self.all_players.append(player)
@@ -64,6 +76,40 @@ class BlackJackGame:
         self.all_players.remove(player)
         if len(self.all_players) == 0:
             self.shutdown_game()
+
+    def move_slot(self, message):
+        user: BlackJackPlayer = message.get("player")
+        new_slot = message.get("new_slot_index")
+        try:
+            old_index = self.sitting_players.index(user)
+        except ValueError:
+            old_index = None
+        target_slot = self.sitting_players[new_slot]
+        if target_slot is None:
+            self.sitting_players[new_slot] = user
+            if old_index is not None:
+                self.sitting_players[old_index] = None
+
+    async def poccess_players_move(self, message_dict):
+        if message_dict.get("messageType", "") == "Action" and (
+            action := message_dict.get("message", None)
+        ):
+            if action == "hit":
+                self.active_hand.add_card(self.deck.get_card())
+                # await self.send_slots()
+                if self.active_hand.is_busted:
+                    return True
+            if action == "stand":
+                return True
+            if action == "double_down":
+                self.active_player.dobule_down_hand(
+                    self.active_hand, self.deck.get_card())
+                # await self.send_slots()
+                return True
+            if action == "split":
+                self.active_player.split_hand(
+                    self.active_hand, self.deck.get_card())
+                # await self.send_slots()
 
     def shutdown_game(self):
         print("Game shut down")
