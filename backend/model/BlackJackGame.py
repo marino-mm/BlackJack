e3382cce23cbdebdf4175b5973a73bd9ec000eb1
Marino <marinovic.matej21@gmail.com>
2025-10-24 17:08:16 +0200

    Startet working on Game class.

--- backend/model/BlackJackGame.py
+++ backend/model/BlackJackGame.py
@@ -1,7 +1,9 @@
 from asyncio import Event, Queue, Task
 from typing import List, Optional, Set
 from backend.model.BlackJack_game_models import Deck, Hand, House
-from backend.model.PlayerMessage import BlackJackPlayer
+from backend.model.BlackJackPlayer import BlackJackPlayer, GameState
+from backend.model.PlayerMessage import PlayerMessageTypeEnum
+from asyncio import create_task as ct
 
 
 class BlackJackGame:
@@ -31,34 +33,35 @@ class BlackJackGame:
     async def game_worker(self):
         while True:
             message = await self.game_queue.get()
-            if message.type == "Join":
+            if message.type == PlayerMessageTypeEnum.JOIN:
                 await self.add_player(message.player)
-            elif message.type == "Disconect":
+            elif message.type == PlayerMessageTypeEnum.DISCONNECT:
                 await self.remove_player(message.player)
-            elif message.type == "MoveSlots":
+            elif message.type == PlayerMessageTypeEnum.MOVE:
                 if message.data.get("new_slot_index") is not None:
                     self.move_slot(message.data)
-                    # await self.send_slots()
-            elif message.type == "PlayerAction":
+            elif message.type == PlayerMessageTypeEnum.HAND_ACTION:
                 if message.player == self.active_player:
                     await self.poccess_players_move(message.data)
+            elif message.type == PlayerMessageTypeEnum.UNKNOWN:
+                continue
+            self.send_update_partial()
+            
 
     async def add_player(self, player: BlackJackPlayer):
         self.all_players.append(player)
-        # await self.send_slots()
-        # await self.send_countdown_time()
-        # await self.send_game_title()
-        # await self.send_house_hand(full=False)
+        self.send_update_partial(player)
 
         if self.game_status == "waiting":
             self.game_status = "game_running"
-            # game_running_task = ct(self._game_running(), name="game_1_task")
-            # self.running_tasks.add(game_running_task)
+            game_running_task = ct(self.game_worker(), name="game_1_task")
+            self.running_tasks.add(game_running_task)
 
     async def remove_player(self, player: BlackJackPlayer):
         self.all_players.remove(player)
         if len(self.all_players) == 0:
             self.shutdown_game()
+        self.send_update_partial()
 
     def move_slot(self, message):
         user: BlackJackPlayer = message.get("player")
@@ -77,21 +80,35 @@ class BlackJackGame:
         if message_dict.get("messageType", "") == "Action" and (action := message_dict.get("message", None)):
             if action == "hit":
                 self.active_hand.add_card(self.deck.get_card())
-                # await self.send_slots()
                 if self.active_hand.is_busted:
                     self._next_hand.set()
             if action == "stand":
                 self._next_hand.set()
             if action == "double_down":
                 self.active_player.dobule_down_hand(self.active_hand, self.deck.get_card())
-                # await self.send_slots()
                 self._next_hand.set()
             if action == "split":
                 self.active_player.split_hand(self.active_hand, self.deck.get_card())
-                # await self.send_slots()
+            self.send_update_partial()
 
     def shutdown_game(self):
         print("Game shut down")
         self.game_status = "waiting"
         for running_task in self.running_tasks:
             running_task.cancel()
+
+    def send_update_partial(self, player: Optional[BlackJackPlayer] = None):
+        game_state = GameState.build_partial(self)
+        if player is not None:
+            player.send(game_state)
+        else:
+            for temp_player in self.all_players:
+                temp_player.send(game_state)
+    
+    def send_update_full(self, player: Optional[BlackJackPlayer] = None):
+        game_state = GameState.build_full(self)
+        if player is not None:
+            player.send(game_state)
+        else:
+            for temp_player in self.all_players:
+                temp_player.send(game_state)
--- backend/model/BlackJackGameState.py
+++ backend/model/BlackJackGameState.py
@@ -2,6 +2,8 @@ from typing import List, Optional
 
 from pydantic import BaseModel
 
+from backend.model.BlackJackGame import BlackJackGame
+
 
 class GameState(BaseModel):
     event_name: Optional[str]
@@ -9,3 +11,35 @@ class GameState(BaseModel):
     slot_list: Optional[List[dict]]
     house_hand: Optional[dict]
     time_remaining: Optional[int]
+
+    @classmethod
+    def build_full(cls, game: BlackJackGame, full_house_hand=False):
+        event_name = game.game_title
+        active_player_username = game.active_player.player_name if game.active_player is not None else ""
+        slot_list = [{"name": player.player_name, "hands": player.hands_json()} if player is not None else None for player in game.sitting_players]
+        house_hand = game.house.hands_json()
+        time_remaining = game.countdown_time
+
+        return cls(
+            event_name=event_name,
+            active_player_username=active_player_username,
+            slot_list=slot_list,
+            house_hand=house_hand,
+            time_remaining=time_remaining,
+        )
+
+    @classmethod
+    def build_partial(cls, game: BlackJackGame, full_house_hand=False):
+        event_name = game.game_title
+        active_player_username = game.active_player.player_name if game.active_player is not None else ""
+        slot_list = [{"name": player.player_name, "hands": player.hands_json()} if player is not None else None for player in game.sitting_players]
+        house_hand = game.house.partial_hand_json()
+        time_remaining = game.countdown_time
+
+        return cls(
+            event_name=event_name,
+            active_player_username=active_player_username,
+            slot_list=slot_list,
+            house_hand=house_hand,
+            time_remaining=time_remaining,
+        )
